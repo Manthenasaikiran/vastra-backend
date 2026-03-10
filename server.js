16bcf3f34695bd0fdad38233c1e9c82cf7724284
manthenasaikiran <saikiranmanthena01@gmail.com>
2026-03-10 12:09:24 +0530

    fixed mongodb connection

--- server.js
+++ server.js
@@ -25,15 +25,12 @@ app.use(express.json());
 
 /* ================= DATABASE ================= */
 
-mongoose.connect(process.env.MONGO_URI,{
-  useNewUrlParser:true,
-  useUnifiedTopology:true
-})
-.then(()=>{
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
   console.log("✅ MongoDB Connected");
 })
-.catch((err)=>{
-  console.log("❌ MongoDB Error:",err);
+.catch((err) => {
+  console.log("❌ MongoDB Error:", err);
   process.exit(1);
 });
 
